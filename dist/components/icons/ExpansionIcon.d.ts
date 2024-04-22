0a61bcd8ede272319398eb173c32272aee44bb78
Laurentius Michael <167497692+mikhaellaurentius@users.noreply.github.com>
2024-04-22 10:51:06 +0700

    Create ExpansionIcon.d.ts

---
+++ dist/components/icons/ExpansionIcon.d.ts
@@ -0,0 +1,3 @@
+import { JSX } from 'solid-js/jsx-runtime';
+export declare const ExpansionIcon: (props: JSX.SvgSVGAttributes<SVGSVGElement>) => JSX.Element;
+//# sourceMappingURL=ExpansionIcon.d.ts.map
